f20442645c0618d9d75895fca47f18149695f946
mattflippa32 <a1799958@student.adelaide.edu.au>
2022-05-02 20:14:33 +0930

    test2

--- main.cpp
+++ main.cpp
@@ -4,34 +4,43 @@
 #include "BitFlipProb.h"
 #include "Rearrange.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+Individual * execute(Individual * indPtr, Mutator * mPtr, int k) {
+    Individual * newPtr = new Individual(1);
+    *newPtr = mPtr->mutate(* indPtr, k);
+    return newPtr;
+}
+
 int main() {
-    vector<int> binaryString = {0,1,1,0,1,1,1,0};
-    Individual dna1(binaryString);
-    string s1 = dna1.getString();
-    cout<<s1<<endl;
-    dna1.flipBit(4);
-    int n = dna1.getBit(4);
-    cout<<n<<endl;
-    int ones = dna1.getMaxOnes();
-    int length = dna1.getLength();
-    cout<<ones<<" "<<length<<endl;
-    
     BitFlip bf;
-    Rearrange r;
-    Individual dna2 = bf.mutate(dna1, 5);
-    string s2 = dna2.getString();
-    cout<<s2<<endl;
-    Individual dna3 = r.mutate(dna2, 3);
-    string s3 = dna3.getString();
-    cout<<s3<<endl;
-    Individual dna4(10);
-    BitFlipProb bfp(0.5);
-    Individual dna5 = bfp.mutate(dna4, 1);
-    string s5 = dna5.getString();
-    cout<<s5<<endl;
+    Mutator * mPtr1;
+    mPtr1 = &bf;
+
+    Rearrange ra;
+    Mutator * mPtr2;
+    mPtr2 = &ra;
+
+    vector<int> binaryString1 = {0,0,0,0,0,0};
+    vector<int> binaryString2 = {0,1,1,1};
+    int k1 = 2;
+    int k2 = 2;
+
+    Individual parent1(binaryString1);
+    Individual * iPtr1 = &parent1;
+    Individual parent2(binaryString2);
+    Individual * iPtr2 = &parent2;
+    
+    
+    Individual * newString1 = execute(iPtr1, mPtr1, k1);
+    Individual * newString2 = execute(iPtr2, mPtr2, k2);
+
+    string s1 = newString1->getString();
+    string s2 = newString2->getString();
+    int ones = newString2->getMaxOnes();
+    cout << s1 << " " << s2 << " " << ones << endl;
 
     return 0;
 }
